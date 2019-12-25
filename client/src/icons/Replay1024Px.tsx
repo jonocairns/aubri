7591e33cb4db9107429a04850789ecaaf3c62d3a
Jono Cairns <jonocairns@gmail.com>
2019-12-25 17:36:17 +1300

    adding skip files

---
+++ client/src/icons/Replay1024Px.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+export const SvgReplay1024Px = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+    <defs>
+      <path id="replay_10-24px_svg__a" d="M0 0h24v24H0V0z" />
+    </defs>
+    <clipPath id="replay_10-24px_svg__b">
+      <use xlinkHref="#replay_10-24px_svg__a" overflow="visible" />
+    </clipPath>
+    <path
+      d="M12 5V1L7 6l5 5V7c3.3 0 6 2.7 6 6s-2.7 6-6 6-6-2.7-6-6H4c0 4.4 3.6 8 8 8s8-3.6 8-8-3.6-8-8-8zm-1.1 11H10v-3.3L9 13v-.7l1.8-.6h.1V16zm4.3-1.8c0 .3 0 .6-.1.8l-.3.6s-.3.3-.5.3-.4.1-.6.1-.4 0-.6-.1-.3-.2-.5-.3-.2-.3-.3-.6-.1-.5-.1-.8v-.7c0-.3 0-.6.1-.8l.3-.6s.3-.3.5-.3.4-.1.6-.1.4 0 .6.1c.2.1.3.2.5.3s.2.3.3.6.1.5.1.8v.7zm-.9-.8v-.5s-.1-.2-.1-.3-.1-.1-.2-.2-.2-.1-.3-.1-.2 0-.3.1l-.2.2s-.1.2-.1.3v2s.1.2.1.3.1.1.2.2.2.1.3.1.2 0 .3-.1l.2-.2s.1-.2.1-.3v-1.5z"
+      clipPath="url(#replay_10-24px_svg__b)"
+    />
+  </svg>
+);
